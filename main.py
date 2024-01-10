ba89744d3494d807d98ed3ae6df583c1b3e9b3d2
TheMacabreKing0909 <155265426+TheMacabreKing0909@users.noreply.github.com>
2024-01-10 02:15:21 +0000

    List of commands

--- main.py
+++ main.py
@@ -4,3 +4,21 @@ print('Hello,',name)
 age=input('Please enter your age.')
 print('You are',age,'years old.')
 print('How may I assist you today',name,'?')
+list=input('Would you like to see a list of commands?')
+if list=='no':
+  print('Okay, have a nice day!')
+if list=='yes':
+  print('Here is a list of commands:')
+  print('1.        ')
+  print('2.        ')
+  print('3.        ')
+  print('4.Generate exit')
+  commands=input('Please enter the number of the command you would like to use.')
+  if commands=='1':
+    print('')
+  elif commands=='2':
+    print('')
+  elif commands=='3':
+    print('')
+  elif commands=='4':
+    print('Okay, have a nice day!')
